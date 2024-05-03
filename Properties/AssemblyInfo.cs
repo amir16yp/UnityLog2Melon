95102078c30b7fd6019bbc1d25fff94cffada593
Amir Y. Perehodnik <myrprhwdnyq@gmail.com>
2024-05-03 19:38:30 +0300

    1.0

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using UnityLog2Melon;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UnityLog2Melon")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnityLog2Melon")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: MelonInfo(typeof(Unity2Melon), "UnityLog2Melon", "1.0", "amir16yp")]
+[assembly: MelonGame(null, null)]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8cfed7dd-6f06-450e-89ce-a23002c24fef")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Unity2Melon.cs
@@ -0,0 +1,32 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+
+
+namespace UnityLog2Melon
+{
+    public class Unity2Melon : MelonMod
+    {
+        public static MelonLogger.Instance logger;
+        public override void OnInitializeMelon()
+        {
+            base.OnInitializeMelon();
+            logger = LoggerInstance;
+            HarmonyInstance.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Debug))]
+        [HarmonyPatch(nameof(Debug.Log))]
+        [HarmonyPatch(new System.Type[]
+        {
+            typeof(object)
+        })]
+        public static class DebugLogPatch
+        {
+            public static void Postfix(ref object message)
+            {
+                logger.Msg(message);
+            }
+        }
+    }
+}
